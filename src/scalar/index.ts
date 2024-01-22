260c2feb6b0c36051bed1a85d9c76435ba96f6eb
marclave <marc@scalar.com>
2024-01-22 14:03:53 -0800

    feat: move to scalar configuration prop

--- src/scalar/index.ts
+++ src/scalar/index.ts
@@ -21,7 +21,7 @@ export const ScalarRender = (version: string, config: ReferenceConfiguration) =>
   <body>
     <script
       id="api-reference"
-      data-url="${config.spec?.url}"></script>
+      data-configuration="JSON.stringify(${Bun.inspect(config)})"></script>
     <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference@${version}/dist/browser/standalone.min.js"></script>
   </body>
 </html>`
