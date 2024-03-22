1348638cf8d5099f835a7a2d457f429450e66ea2
hgpark <hanguyl91@gmail.com>
2024-03-22 12:25:15 +0900

    fix: move parameter schema keywords location

--- src/utils.ts
+++ src/utils.ts
@@ -25,11 +25,11 @@ export const mapProperties = (
         else throw new Error(`Can't find model ${schema}`)
 
     return Object.entries(schema?.properties ?? []).map(([key, value]) => {
-        const { type: valueType = undefined, ...rest } = value as any
+        const { type: valueType = undefined, description, examples, ...schemaProperties } = value as any
         return {
             // @ts-ignore
-            ...rest,
-            schema: { type: valueType },
+            description, examples,
+            schema: { type: valueType, ...schemaProperties },
             in: name,
             name: key,
             // @ts-ignore
