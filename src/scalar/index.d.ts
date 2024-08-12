40c1267dbd4d486e85bb4cddfe70e53078fe7032
saltyaom <saltyaom@gmail.com>
2024-08-12 23:47:27 +0700

    :tada: release: 1.1.1

--- src/scalar/index.d.ts
+++
@@ -1,2 +0,0 @@
-import type { ReferenceConfiguration } from './types';
-export declare const ScalarRender: (version: string, config: ReferenceConfiguration, cdn: string) => string;
--- src/swagger/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { OpenAPIV3 } from 'openapi-types';
-export declare const SwaggerUIRender: (info: OpenAPIV3.InfoObject, version: string, theme: string | {
-    light: string;
-    dark: string;
-}, stringifiedSwaggerOptions: string, autoDarkMode?: boolean) => string;
