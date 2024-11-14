8b39445c04d69cd6e37b651125f5721e460491ea
André Felipe Schulle <andrefelipeschulle@icloud.com>
2024-11-14 17:50:38 -0300

    fix example, create archive with example

--- example/index2.ts
+++ example/index2.ts
@@ -1,35 +1,44 @@
 import { Elysia } from 'elysia'
 import { swagger } from '../src/index'
-
-const firstApp = new Elysia({ prefix: '/first' })
-
-firstApp
-	.get('/first-route', () => {
-		return 'first route!'
-	})
-	.use(
-		swagger({
-			path: '/first-doc',
-			routes: firstApp.routes
-		})
-	)
-
-const secondApp = new Elysia({ prefix: '/second' })
-
-secondApp
-	.get('/second-route', () => {
-		return 'second route!'
-	})
-	.use(
-		swagger({
-			path: '/second-doc',
-			routes: secondApp.routes
-		})
-	)
+import { plugin } from './plugin'
 
 const app = new Elysia({
 	// aot: false
 })
-	.use(firstApp)
-	.use(secondApp)
+	.use(
+		swagger({
+			documentation: {
+				info: {
+					title: 'Elysia',
+					version: '0.6.10'
+				},
+				tags: [
+					{
+						name: 'Test',
+						description: 'Hello'
+					}
+				],
+				security: [{ JwtAuth: [] }],
+				components: {
+					schemas: {
+						User: {
+							description: 'string'
+						}
+					},
+					securitySchemes: {
+						JwtAuth: {
+							type: 'http',
+							scheme: 'bearer',
+							bearerFormat: 'JWT',
+							description: 'Enter JWT Bearer token **_only_**'
+						}
+					}
+				}
+			},
+			swaggerOptions: {
+				persistAuthorization: true
+			}
+		})
+	)
+	.use(plugin)
 	.listen(3000)
